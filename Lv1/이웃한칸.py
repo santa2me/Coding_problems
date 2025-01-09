d7b4b67424f5396fa193800d5d5640d674931913
santa2me <santa2yugyeong@gmail.com>
2025-01-09 14:28:09 +0900

    이웃한 칸

---
+++ Lv1/이웃한칸.py
@@ -0,0 +1,16 @@
+def solution(board, h, w):
+    answer = 0
+    n = len(board)
+    dh = [0,1,-1,0]
+    dw = [1,0,0,-1]
+    
+    for i in range(4):
+        h_check = h + dh[i]
+        w_check = w + dh[i]
+        
+        if 0 <= h_check < n and 0 <= w_check < n:
+            if board[h][w] == board[h_check][w_check]:
+                answer += 1
+    return answer
+
+solution([["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]], 1, 1)
